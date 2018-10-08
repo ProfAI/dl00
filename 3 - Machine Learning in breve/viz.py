155321b02da9b5acb29f864db6313c1135e86735
gfgullo <gfgullo@gmail.com>
2018-10-08 16:46:54 +0200

    first commit

---
+++ 3 - Machine Learning in breve/viz.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def showBounds(X,Y,model, labels=["classe negativa", "classe positiva"]):
+    
+    h = .02 
+
+    x_min, x_max = X[:, 0].min(), X[:, 0].max()
+    y_min, y_max = X[:, 1].min(), X[:, 1].max()
+
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+
+    Z = model.predict_classes(np.c_[xx.ravel(), yy.ravel()])
+
+    Z = Z.reshape(xx.shape)
+    plt.contourf(xx, yy, Z, cmap=plt.cm.Paired)
+
+    X_m = X[Y==1]
+    X_b = X[Y==0]
+    plt.scatter(X_b[:, 0], X_b[:, 1], c="green",  edgecolor='white', label=labels[0])
+    plt.scatter(X_m[:, 0], X_m[:, 1], c="red",  edgecolor='white', label=labels[1])
